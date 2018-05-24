02faa39a26eaecd536652eebee98be16a2269389
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2018-05-24 15:35:42 -0600

    attempted to finish lab4

--- lab4/NonRecursive.java
+++ lab4/NonRecursive.java
@@ -2,9 +2,10 @@ public class NonRecursive extends SortFactory{
     String algoName;
 
     public NonRecursive(){
+      //need to get super class varaible algorithm
         this.algorithm = algoName;
     }
-    
+
     public SortingAlgorithm getAlgorithm(){
         SortingAlgorithm sortingAlgorithm = null;
         if(algoName.equals("Selection"))
@@ -12,6 +13,6 @@ public class NonRecursive extends SortFactory{
         else if(algoName.equals("Insertion"))
             sortingAlgorithm = new InsertionSort();
         return sortingAlgorithm;
-  
-    }  
+
+    }
 }
